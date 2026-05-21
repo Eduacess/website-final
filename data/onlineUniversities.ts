930b6c7bc1f4731e86eac3e463c5797e9158215e
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 03:05:49 +0530

    uni

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -1,319 +1,323 @@
-{
-  name: 'Amity University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/0/0f/Amity_University_logo.png',
-  programs: {
-    ug: ['BBA', 'BCA', 'BCom'],
-    pg: ['MBA', 'MCA', 'MCom'],
-    more: '50+ More Programs',
-  },
-  benefits: [
-    {
-      title: 'UGC Approved',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-    },
-    {
-      title: 'NAAC A+',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
-    },
-    {
-      title: 'Global Degrees',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-    {
-      title: 'Industry Ready',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
-    },
-  ],
-},
+export const universities = [
 
-{
-  name: 'Manipal University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/9/95/Manipal_University_Jaipur_Logo.png',
-  programs: {
-    ug: ['BBA', 'BCom', 'BCA'],
-    pg: ['MBA', 'MCA'],
-    more: '40+ More Programs',
+  {
+    name: 'Amity University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/0/0f/Amity_University_logo.png',
+    programs: {
+      ug: ['BBA', 'BCA', 'BCom'],
+      pg: ['MBA', 'MCA', 'MCom'],
+      more: '50+ More Programs',
+    },
+    benefits: [
+      {
+        title: 'UGC Approved',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'NAAC A+',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+      },
+      {
+        title: 'Global Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Industry Ready',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'UGC Approved',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+  
+  {
+    name: 'Manipal University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/9/95/Manipal_University_Jaipur_Logo.png',
+    programs: {
+      ug: ['BBA', 'BCom', 'BCA'],
+      pg: ['MBA', 'MCA'],
+      more: '40+ More Programs',
     },
-    {
-      title: 'Top Ranked',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
-    },
-    {
-      title: 'Placement Support',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-    {
-      title: 'Digital Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-  ],
-},
-
-{
-  name: 'NMIMS University',
-  type: 'PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/0/0d/NMIMS_Logo.png',
-  programs: {
-    ug: [],
-    pg: ['MBA', 'PGDM', 'MCom'],
-    more: '25+ More Programs',
+    benefits: [
+      {
+        title: 'UGC Approved',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Top Ranked',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+      },
+      {
+        title: 'Placement Support',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+      {
+        title: 'Digital Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'AACSB Accredited',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+  
+  {
+    name: 'NMIMS University',
+    type: 'PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/0/0d/NMIMS_Logo.png',
+    programs: {
+      ug: [],
+      pg: ['MBA', 'PGDM', 'MCom'],
+      more: '25+ More Programs',
     },
-    {
-      title: 'Premium Business School',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
-    },
-    {
-      title: 'Global Recognition',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-    {
-      title: 'Industry Curriculum',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
-    },
-  ],
-},
-
-{
-  name: 'Jain University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/e/e5/Jain_University_Logo.png',
-  programs: {
-    ug: ['BBA', 'BCA', 'BA'],
-    pg: ['MBA', 'MCA', 'MA'],
-    more: '45+ More Programs',
+    benefits: [
+      {
+        title: 'AACSB Accredited',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+      },
+      {
+        title: 'Premium Business School',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+      },
+      {
+        title: 'Global Recognition',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Industry Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'NAAC A++',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+  
+  {
+    name: 'Jain University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/e/e5/Jain_University_Logo.png',
+    programs: {
+      ug: ['BBA', 'BCA', 'BA'],
+      pg: ['MBA', 'MCA', 'MA'],
+      more: '45+ More Programs',
     },
-    {
-      title: 'Global Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-    {
-      title: 'Modern LMS',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-    {
-      title: 'Career Support',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-  ],
-},
-
-{
-  name: 'LPU University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/3/32/Lovely_Professional_University_logo.png',
-  programs: {
-    ug: ['BCA', 'BBA', 'BA'],
-    pg: ['MBA', 'MCA'],
-    more: '60+ More Programs',
+    benefits: [
+      {
+        title: 'NAAC A++',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Global Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Modern LMS',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Career Support',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'NAAC A++',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+  
+  {
+    name: 'LPU University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/3/32/Lovely_Professional_University_logo.png',
+    programs: {
+      ug: ['BCA', 'BBA', 'BA'],
+      pg: ['MBA', 'MCA'],
+      more: '60+ More Programs',
     },
-    {
-      title: 'Global Recognition',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-    {
-      title: 'Advanced LMS',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-    {
-      title: 'Career Support',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-  ],
-},
-
-{
-  name: 'Chandigarh University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/9/92/Chandigarh_University_seal.png',
-  programs: {
-    ug: ['BBA', 'BCA'],
-    pg: ['MBA', 'MCA'],
-    more: '40+ More Programs',
+    benefits: [
+      {
+        title: 'NAAC A++',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Global Recognition',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Advanced LMS',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Career Support',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'International Collaborations',
-      icon: 'https://cdn-icons-png.flaticon.com/512/4140/4140048.png',
+  
+  {
+    name: 'Chandigarh University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/9/92/Chandigarh_University_seal.png',
+    programs: {
+      ug: ['BBA', 'BCA'],
+      pg: ['MBA', 'MCA'],
+      more: '40+ More Programs',
     },
-    {
-      title: 'Modern Digital Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-    {
-      title: 'UGC Approved',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-    },
-    {
-      title: 'Career Ready',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-  ],
-},
-
-{
-  name: 'DY Patil University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/e/e0/DY_Patil_University_logo.png',
-  programs: {
-    ug: ['BBA', 'BCA'],
-    pg: ['MBA', 'MCA'],
-    more: '35+ More Programs',
+    benefits: [
+      {
+        title: 'International Collaborations',
+        icon: 'https://cdn-icons-png.flaticon.com/512/4140/4140048.png',
+      },
+      {
+        title: 'Modern Digital Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'UGC Approved',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Career Ready',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'NAAC A++',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+  
+  {
+    name: 'DY Patil University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/e/e0/DY_Patil_University_logo.png',
+    programs: {
+      ug: ['BBA', 'BCA'],
+      pg: ['MBA', 'MCA'],
+      more: '35+ More Programs',
     },
-    {
-      title: 'Flexible Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-    {
-      title: 'Industry Focused',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
-    },
-    {
-      title: 'Placement Assistance',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-  ],
-},
-
-{
-  name: 'UPES University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/3/3b/UPES_Logo.png',
-  programs: {
-    ug: ['BBA', 'BCA'],
-    pg: ['MBA'],
-    more: '30+ More Programs',
+    benefits: [
+      {
+        title: 'NAAC A++',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Flexible Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Focused',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Placement Assistance',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'Industry Aligned',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+  
+  {
+    name: 'UPES University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/3/3b/UPES_Logo.png',
+    programs: {
+      ug: ['BBA', 'BCA'],
+      pg: ['MBA'],
+      more: '30+ More Programs',
     },
-    {
-      title: 'UGC Approved',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-    },
-    {
-      title: 'Career Ready',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-    {
-      title: 'Modern Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-  ],
-},
-
-{
-  name: 'Shoolini University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/f/f8/Shoolini_University_logo.png',
-  programs: {
-    ug: ['BBA', 'BA'],
-    pg: ['MBA', 'MA'],
-    more: '20+ More Programs',
+    benefits: [
+      {
+        title: 'Industry Aligned',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'UGC Approved',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Career Ready',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+      {
+        title: 'Modern Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'Research Focused',
-      icon: 'https://cdn-icons-png.flaticon.com/512/4140/4140047.png',
+  
+  {
+    name: 'Shoolini University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/f/f8/Shoolini_University_logo.png',
+    programs: {
+      ug: ['BBA', 'BA'],
+      pg: ['MBA', 'MA'],
+      more: '20+ More Programs',
     },
-    {
-      title: 'Global Collaborations',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-    {
-      title: 'NAAC Accredited',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-    },
-    {
-      title: 'Modern Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-  ],
-},
-
-{
-  name: 'Sikkim Manipal University',
-  type: 'UG + PG',
-  flag: 'https://flagcdn.com/w40/in.png',
-  location: 'India',
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/3/3e/Sikkim_Manipal_University_logo.png',
-  programs: {
-    ug: ['BBA', 'BA'],
-    pg: ['MBA', 'MA'],
-    more: '25+ More Programs',
+    benefits: [
+      {
+        title: 'Research Focused',
+        icon: 'https://cdn-icons-png.flaticon.com/512/4140/4140047.png',
+      },
+      {
+        title: 'Global Collaborations',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'NAAC Accredited',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Modern Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+    ],
   },
-  benefits: [
-    {
-      title: 'UGC Entitled',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+  
+  {
+    name: 'Sikkim Manipal University',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/3/3e/Sikkim_Manipal_University_logo.png',
+    programs: {
+      ug: ['BBA', 'BA'],
+      pg: ['MBA', 'MA'],
+      more: '25+ More Programs',
     },
-    {
-      title: 'Flexible Learning',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
-    },
-    {
-      title: 'Affordable Programs',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
-    },
-    {
-      title: 'Career Support',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-  ],
-},
+    benefits: [
+      {
+        title: 'UGC Entitled',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Flexible Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Affordable Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+      },
+      {
+        title: 'Career Support',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+    ],
+  },
+  
+  ];
