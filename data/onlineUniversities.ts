8fdc9bf210652bf0eb10ccecde87bf4bdbc2fc31
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 19:09:23 +0530

    past

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -2,14 +2,16 @@ export const universities = [
 
   {
     name: 'Amity Online',
-
     type: 'UG + PG',
 
     flag: 'https://flagcdn.com/w40/in.png',
 
     location: 'India',
 
-    image: '/amity.png',
+    image:
+      'https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1400&auto=format&fit=crop',
+
+    logo: '/amity.png',
 
     programs: {
 
@@ -33,22 +35,26 @@ export const universities = [
 
       {
         title: 'NAAC A+ Accredited',
-        icon: 'https://cdn-icons-png.flaticon.com/512/5610/5610944.png',
+        icon:
+          'https://cdn-icons-png.flaticon.com/512/190/190411.png',
       },
 
       {
-        title: 'THE Ranked',
-        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+        title: 'THE Ranked University',
+        icon:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/THE_World_University_Rankings_logo.svg/512px-THE_World_University_Rankings_logo.svg.png',
       },
 
       {
-        title: 'QS Ranked',
-        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+        title: 'QS Ranked University',
+        icon:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/QS_World_University_Rankings_logo.svg/512px-QS_World_University_Rankings_logo.svg.png',
       },
 
       {
         title: 'Placement Opportunities',
-        icon: 'https://cdn-icons-png.flaticon.com/512/942/942799.png',
+        icon:
+          'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
       },
 
     ],
