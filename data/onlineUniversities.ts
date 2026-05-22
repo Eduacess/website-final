6f67b244ccbcfce4281d773a13ca2f9608b1310f
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 22:56:19 +0530

    Update onlineUniversities.ts

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -2,57 +2,183 @@ export const universities = [
 
   {
     name: 'Amity Online',
-
     type: 'UG + PG',
-
     flag: 'https://flagcdn.com/w40/in.png',
-
     location: 'India',
-
-    image: '/amity-campus.png',
-
+    image: 'https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1400&auto=format&fit=crop',
     programs: {
-
-      ug: [
-        'BBA',
-        'BCA',
-        'B.Com',
-        'BA Journalism & Mass Communication',
-      ],
-
-      pg: [
-        'MBA',
-        'MCA',
-        'M.Com',
-        'MA Journalism & Mass Communication',
-      ],
-
+      ug: ['BBA', 'BCA', 'B.Com'],
+      pg: ['MBA', 'MCA', 'M.Com'],
     },
-
     benefits: [
-
       {
         title: 'NAAC A+ Accredited',
-        icon: '/naaca+.png',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
       },
-
       {
-        title: 'THE Ranked University',
-        icon: '/the.png',
+        title: 'Top Ranked University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
       },
-
       {
-        title: 'QS Ranked University',
-        icon: '/qs.png',
-      },
-
-      {
-        title: 'Placement Opportunities',
+        title: 'Industry Ready Degrees',
         icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
       },
-
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
     ],
+  },
 
+  {
+    name: 'DY Patil Online',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image: 'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: ['BBA', 'BCA', 'B.Com'],
+      pg: ['MBA', 'MCA'],
+    },
+    benefits: [
+      {
+        title: 'NAAC A++ Accredited',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Digital Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Career Focused',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Manipal Online',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image: 'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: ['BBA', 'BCA', 'B.Com'],
+      pg: ['MBA', 'MCA'],
+    },
+    benefits: [
+      {
+        title: 'Top Ranked University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Flexible Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'NMIMS Online',
+    type: 'PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image: 'https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [],
+      pg: ['MBA', 'PGDM', 'M.Com'],
+    },
+    benefits: [
+      {
+        title: 'Premium Business School',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+      },
+      {
+        title: 'Industry Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Top Ranked University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'SRM Online',
+    type: 'UG + PG',
+    flag: 'https://flagcdn.com/w40/in.png',
+    location: 'India',
+    image: 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: ['BBA', 'BCA'],
+      pg: ['MBA', 'MCA'],
+    },
+    benefits: [
+      {
+        title: 'NAAC Accredited',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+      {
+        title: 'Modern Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Ready',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Others',
+    type: 'Find Your Program',
+    flag: 'https://flagcdn.com/w40/un.png',
+    location: 'Global',
+    image: 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: ['100+ Universities'],
+      pg: ['Online Degrees Worldwide'],
+    },
+    benefits: [
+      {
+        title: 'Global Universities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Expert Counselling',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+      {
+        title: 'Affordable Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3063/3063822.png',
+      },
+      {
+        title: 'Find Right Program',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+      },
+    ],
+    others: true,
   },
 
 ];
