d533c150a2830e2d704818ef88767df7b4b64378
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 16:31:35 +0530

    amity

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -2,55 +2,57 @@ export const universities = [
 
   {
     name: 'Amity Online',
-  
+
     type: 'UG + PG',
-  
+
     flag: 'https://flagcdn.com/w40/in.png',
-  
+
     location: 'India',
-  
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/2/20/Amity_University_logo.png',
-  
+
+    image: '/universities/amity.png',
+
     programs: {
-  
+
       ug: [
         'BBA',
         'BCA',
         'B.Com',
         'BA Journalism & Mass Communication',
       ],
-  
+
       pg: [
         'MBA',
         'MCA',
         'M.Com',
         'MA Journalism & Mass Communication',
       ],
-  
+
     },
-  
+
     benefits: [
-  
+
       {
         title: 'NAAC A+ Accredited',
         icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828640.png',
       },
-  
+
       {
         title: 'THE & QS Ranked University',
         icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
       },
-  
+
       {
         title: 'UGC Entitled Online Degrees',
         icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
       },
-  
+
       {
         title: 'Placement Opportunities',
         icon: 'https://cdn-icons-png.flaticon.com/512/942/942748.png',
       },
-  
+
     ],
+
   },
+
+];
