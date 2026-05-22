77098f466c84787eec4cb877767714cc3ce24ab3
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 21:40:30 +0530

    Update onlineUniversities.ts

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -1,67 +1,54 @@
-export const universities = [
+{
+  name: 'Amity Online',
 
-  {
-    name: 'Amity Online',
+  type: 'UG + PG',
 
-    type: 'UG + PG',
+  flag: 'https://flagcdn.com/w40/in.png',
 
-    flag: 'https://flagcdn.com/w40/in.png',
+  location: 'India',
 
-    location: 'India',
+  image: '/amity-campus.png',
 
-    image:
-      'https://images.collegedunia.com/public/college_data/images/campusimage/14181978011412140322amity.jpg',
+  programs: {
 
-    programs: {
+    ug: [
+      'BBA',
+      'BCA',
+      'B.Com',
+      'BA Journalism & Mass Communication',
+    ],
 
-      ug: [
-        'BBA',
-        'BCA',
-        'B.Com',
-        'BA Journalism & Mass Communication',
-      ],
-
-      pg: [
-        'MBA',
-        'MCA',
-        'M.Com',
-        'MA Journalism & Mass Communication',
-      ],
-
-    },
-
-    benefits: [
-
-      {
-        title: 'NAAC A+ Accredited',
-
-        icon:
-          'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-      },
-
-      {
-        title: 'THE Ranked University',
-
-        icon:
-          'https://upload.wikimedia.org/wikipedia/commons/8/82/THE_World_University_Rankings_logo.svg',
-      },
-
-      {
-        title: 'QS Ranked University',
-
-        icon:
-          'https://upload.wikimedia.org/wikipedia/commons/0/02/QS_World_University_Rankings_logo.svg',
-      },
-
-      {
-        title: 'Placement Opportunities',
-
-        icon:
-          'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
-      },
-
+    pg: [
+      'MBA',
+      'MCA',
+      'M.Com',
+      'MA Journalism & Mass Communication',
     ],
 
   },
 
-];
+  benefits: [
+
+    {
+      title: 'NAAC A+ Accredited',
+      icon: '/naaca+.png',
+    },
+
+    {
+      title: 'THE Ranked University',
+      icon: '/the.png',
+    },
+
+    {
+      title: 'QS Ranked University',
+      icon: '/qs.png',
+    },
+
+    {
+      title: 'Placement Opportunities',
+      icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    },
+
+  ],
+
+},
