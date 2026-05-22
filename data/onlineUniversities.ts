571bad9f221781a741b59287051844e2919e6d55
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 16:03:50 +0530

    amity

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -2,57 +2,55 @@ export const universities = [
 
   {
     name: 'Amity Online',
-
+  
     type: 'UG + PG',
-
+  
     flag: 'https://flagcdn.com/w40/in.png',
-
+  
     location: 'India',
-
+  
     image:
-      'https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Amity_University_logo.svg/1200px-Amity_University_logo.svg.png',
-
+      'https://upload.wikimedia.org/wikipedia/commons/2/20/Amity_University_logo.png',
+  
     programs: {
-
+  
       ug: [
         'BBA',
         'BCA',
         'B.Com',
         'BA Journalism & Mass Communication',
       ],
-
+  
       pg: [
         'MBA',
         'MCA',
         'M.Com',
         'MA Journalism & Mass Communication',
       ],
-
+  
     },
-
+  
     benefits: [
-
+  
       {
         title: 'NAAC A+ Accredited',
-        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828640.png',
       },
-
+  
       {
         title: 'THE & QS Ranked University',
-        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
       },
-
+  
       {
         title: 'UGC Entitled Online Degrees',
         icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
       },
-
+  
       {
         title: 'Placement Opportunities',
-        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+        icon: 'https://cdn-icons-png.flaticon.com/512/942/942748.png',
       },
-
+  
     ],
   },
-
-];
