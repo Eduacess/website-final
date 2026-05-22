fc8fe3c9f0372b9a9cfdd52813808bc606099c18
Eduacess <connect.eduaccess@outlook.com>
2026-05-22 16:00:12 +0530

    au

--- data/onlineUniversities.ts
+++ data/onlineUniversities.ts
@@ -1,55 +1,58 @@
-{
-  name: 'Amity Online',
+export const universities = [
 
-  type: 'UG + PG',
+  {
+    name: 'Amity Online',
 
-  flag: 'https://flagcdn.com/w40/in.png',
+    type: 'UG + PG',
 
-  location: 'India',
+    flag: 'https://flagcdn.com/w40/in.png',
 
-  // Official Amity Online Logo
-  image:
-    'https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Amity_University_logo.svg/1200px-Amity_University_logo.svg.png',
+    location: 'India',
 
-  programs: {
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Amity_University_logo.svg/1200px-Amity_University_logo.svg.png',
 
-    ug: [
-      'BBA',
-      'BCA',
-      'B.Com',
-      'BA Journalism & Mass Communication',
+    programs: {
+
+      ug: [
+        'BBA',
+        'BCA',
+        'B.Com',
+        'BA Journalism & Mass Communication',
+      ],
+
+      pg: [
+        'MBA',
+        'MCA',
+        'M.Com',
+        'MA Journalism & Mass Communication',
+      ],
+
+    },
+
+    benefits: [
+
+      {
+        title: 'NAAC A+ Accredited',
+        icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
+      },
+
+      {
+        title: 'THE & QS Ranked University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
+      },
+
+      {
+        title: 'UGC Entitled Online Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+
+      {
+        title: 'Placement Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
+      },
+
     ],
-
-    pg: [
-      'MBA',
-      'MCA',
-      'M.Com',
-      'MA Journalism & Mass Communication',
-    ],
-
   },
 
-  benefits: [
-
-    {
-      title: 'NAAC A+ Accredited',
-      icon: 'https://cdn-icons-png.flaticon.com/512/190/190411.png',
-    },
-
-    {
-      title: 'THE & QS Ranked University',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2583/2583344.png',
-    },
-
-    {
-      title: 'UGC Entitled Online Degrees',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
-    },
-
-    {
-      title: 'Placement Opportunities',
-      icon: 'https://cdn-icons-png.flaticon.com/512/1048/1048953.png',
-    },
-
-  ],
-},
+];
