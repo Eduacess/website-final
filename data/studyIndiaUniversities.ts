f6879b48d406ead9d5b8d1349c7da3685bfce7be
Eduacess <connect.eduaccess@outlook.com>
2026-05-23 15:31:50 +0530

    Create studyIndiaUniversities.ts

---
+++ data/studyIndiaUniversities.ts
@@ -0,0 +1,589 @@
+export const universities = [
+
+  {
+    name: 'Deakin University India',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/au.png',
+    location: 'GIFT City, Gujarat',
+    image:
+      'https://images.unsplash.com/photo-1562774053-701939374585?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [],
+      pg: [
+        'Master of Business Analytics',
+        'Master of Cyber Security',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Globally Recognised Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Future Ready Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Focused Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Victoria University India',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/au.png',
+    location: 'Delhi NCR, India',
+    image:
+      'https://images.unsplash.com/photo-1541339907198-e08756dedf3f?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Bachelor of Business',
+        'Bachelor of Information Technology',
+        'Bachelor of Data Science',
+        'Bachelor of Cyber Security',
+      ],
+      pg: [
+        'MBA Global',
+        'Master of Applied Information Technology',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Australian University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Global Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Oriented Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'International Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'University of York India',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'Mumbai, India',
+    image:
+      'https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Computer Science',
+        'Business',
+      ],
+      pg: [
+        'Management',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Russell Group University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'International Learning Experience',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Industry Focused Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Career Growth',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Queen’s University Belfast India',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'GIFT City, Gujarat',
+    image:
+      'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [],
+      pg: [
+        'Finance',
+        'Financial Analytics',
+        'Business Analytics',
+        'Construction Project Management',
+        'Artificial Intelligence',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Russell Group University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Future Ready Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Focused Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Seneca Polytechnic',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/ca.png',
+    location: 'Ahmedabad, Gujarat',
+    image:
+      'https://images.unsplash.com/photo-1607237138185-eedd9c632b0b?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Hospitality',
+      ],
+      pg: [
+        'Project Management',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Canadian Institution',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Practical Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Leeds Beckett University',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'Ahmedabad, Gujarat',
+    image:
+      'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business Management',
+        'Hospitality Management',
+      ],
+      pg: [
+        'MBA',
+        'International Business',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Industry Focused Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Global Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'London Metropolitan University',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business Management',
+        'Computer Science',
+      ],
+      pg: [
+        'MBA',
+        'Cyber Security',
+      ],
+    },
+    benefits: [
+      {
+        title: 'London Based University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'International Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Driven Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'De Montfort University Leicester',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1503676260728-1c00da094a0b?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Computing',
+        'Design',
+      ],
+      pg: [
+        'MBA',
+        'Project Management',
+        'Data Analytics',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Globally Recognised Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Oriented Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'UWE Bristol',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1523240795612-9a054b0db644?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Engineering',
+      ],
+      pg: [
+        'MBA',
+        'Data Science',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Global Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Focused Programs',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'International Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'University of Sunderland',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1523580494863-6f3031224c94?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Health Sciences',
+      ],
+      pg: [
+        'MBA',
+        'Public Health',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Modern Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Industry Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'University of Portsmouth',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1513258496099-48168024aec0?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Computing',
+      ],
+      pg: [
+        'MBA',
+        'Artificial Intelligence',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Research Driven Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'University of Central Lancashire',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1517486808906-6ca8b3f04846?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Hospitality',
+      ],
+      pg: [
+        'MBA',
+        'Management',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK Institution',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Global Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Industry Focused Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Liverpool John Moores University',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1544531585-9847b68c8c86?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Engineering',
+      ],
+      pg: [
+        'MBA',
+        'Project Management',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Globally Recognised Degrees',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Industry Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3281/3281289.png',
+      },
+      {
+        title: 'International Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Career Growth',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Canterbury Christ Church University',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'India',
+    image:
+      'https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business',
+        'Psychology',
+      ],
+      pg: [
+        'MBA',
+        'Education',
+      ],
+    },
+    benefits: [
+      {
+        title: 'UK University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Flexible Learning',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'Global Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'Edith Cowan University',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/au.png',
+    location: 'Mysore, Karnataka',
+    image:
+      'https://images.unsplash.com/photo-1564981797816-1043664bf78d?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'BSc Nursing (2 Years India + 2 Years Australia)',
+      ],
+      pg: [],
+    },
+    benefits: [
+      {
+        title: 'International Nursing Program',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3209/3209265.png',
+      },
+      {
+        title: 'Study In India With International Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Future Ready Healthcare Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Global Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+  {
+    name: 'University of Southampton Delhi',
+    type: 'Study In India',
+    flag: 'https://flagcdn.com/w40/gb.png',
+    location: 'Delhi NCR, India',
+    image:
+      'https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?q=80&w=1400&auto=format&fit=crop',
+    programs: {
+      ug: [
+        'Business Management',
+        'Economics',
+      ],
+      pg: [
+        'Finance',
+      ],
+    },
+    benefits: [
+      {
+        title: 'Russell Group University',
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png',
+      },
+      {
+        title: 'Global Curriculum',
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+      {
+        title: 'International Exposure',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png',
+      },
+      {
+        title: 'Career Opportunities',
+        icon: 'https://cdn-icons-png.flaticon.com/512/2910/2910791.png',
+      },
+    ],
+  },
+
+];
