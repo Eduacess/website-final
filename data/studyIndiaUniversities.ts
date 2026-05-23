e5b774c9edfe14d2b0da2ba1956644997ee7d821
Eduacess <connect.eduaccess@outlook.com>
2026-05-23 16:20:57 +0530

    Create studyIndiaUniversities.ts

---
+++ data/studyIndiaUniversities.ts
@@ -0,0 +1,28 @@
+export const universities = [
+  {
+    name: 'Test University',
+
+    type: 'Study In India',
+
+    flag: 'https://flagcdn.com/w40/in.png',
+
+    location: 'India',
+
+    image:
+      'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=1400&auto=format&fit=crop',
+
+    programs: {
+      ug: ['BBA'],
+      pg: ['MBA'],
+    },
+
+    benefits: [
+      {
+        title: 'Global Exposure',
+
+        icon:
+          'https://cdn-icons-png.flaticon.com/512/3135/3135789.png',
+      },
+    ],
+  },
+];
